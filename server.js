92d456443af0fc42f8ea241d52a6186dd3896da4
Brian Jang <brian.jang91@gmail.com>
2017-03-27 16:33:36 -0400

    Add webpack configuration, and add express configuration

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 // Set up express and port
-import express from "express";
-import bodyParse from "bodyParse";
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+// Middleware
+app.use(bodyParser.json());
+
+// Get loan info
+app.get("/", (req, res) => {
+  res.send("Let's help save the world!");
+});
+
+// Start app with "node server.js" with anonymous callback to log
+// port information
+app.listen(port, () => {
+  console.log("App started on port " + port);
+});
