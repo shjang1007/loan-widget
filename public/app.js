0ccc888b6c3e185042ddbd6a66224c2d3150119e
Brian Jang <brian.jang91@gmail.com>
2017-03-29 15:50:58 -0400

    Add flip style to flip form and chart

--- public/app.js
+++ public/app.js
@@ -14,47 +14,22 @@ app.factory("intRates", ["$http", ($http) => {
 }]);
 
 app.controller("MainCtrl", ["$scope", "intRates", ($scope, intRates) => {
+  // Variables to help toggle show and class
+  $scope.invalidSubmit = false;
+  $scope.isFlipped = false;
+
+  // Function that will handle form submission.
+  $scope.handleSubmit = () => {
+    if ($scope.loanAmount && $scope.interestRate && $scope.loanPeriod) {
+      $scope.invalidSubmit = false;
+      $scope.isFlipped = true;
+    } else {
+      $scope.invalidSubmit = true;
+    }
+  };
+
   intRates.get()
     .then( (response) => {
       $scope.intRates = response.data;
     });
 }]);
-
-
-// angular.module('todoController', [])
-//
-// 	// inject the Todo service factory into our controller
-// 	.controller('mainController', ['$scope','$http','Todos', function($scope, $http, Todos) {
-// 		$scope.formData = {};
-// 		$scope.loading = true;
-//
-// 		// GET =====================================================================
-// 		// when landing on the page, get all todos and show them
-// 		// use the service to get all the todos
-// 		Todos.get()
-// 			.success(function(data) {
-// 				$scope.todos = data;
-// 				$scope.loading = false;
-// 			});
-//
-// 		// CREATE ==================================================================
-// 		// when submitting the add form, send the text to the node API
-// 		$scope.createTodo = function() {
-//
-// 			// validate the formData to make sure that something is there
-// 			// if form is empty, nothing will happen
-// 			if ($scope.formData.text != undefined) {
-// 				$scope.loading = true;
-//
-// 				// call the create function from our service (returns a promise object)
-// 				Todos.create($scope.formData)
-//
-// 					// if successful creation, call our get function to get all the new todos
-// 					.success(function(data) {
-// 						$scope.loading = false;
-// 						$scope.formData = {}; // clear the form so our user is ready to enter another
-// 						$scope.todos = data; // assign our new list of todos
-// 					});
-// 			}
-// 		};
-// 	}]);
